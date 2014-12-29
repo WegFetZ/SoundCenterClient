89d274f72fd16527b81c9d3e7aeb806f14620eac
WegFetZ <mweberwen@gmail.com>
2014-12-29 11:03:09 +0100

    update version number

--- SoundCenterClient/src/com/soundcenter/soundcenter/client/App.java
+++ SoundCenterClient/src/com/soundcenter/soundcenter/client/App.java
@@ -12,7 +12,7 @@ import com.soundcenter.soundcenter.client.util.SCLogger;
 
 public class App {
 
-	public static final double version = 0.300;
+	public static final double version = 0.301;
 	public static String dataFolder = "";
 	public static UserInterface gui = null;
 	public static SCLogger logger = null;
